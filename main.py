89f18841a712e93ba4f222345ea16ccd95fc8def
Chloe Calvarin <chloe.calvarin@gmail.com>
2016-04-12 16:35:44 -0700

    added sentences for project 1.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Here, we get the training data & evaluate our parsers
 """
-
+import nltk
 from nltk.corpus import treebank
 
 # Get the Penn Treebank corpus
@@ -10,3 +10,6 @@ files = files[:5] # make shorter for setup
 
 parsed_sents = [sent for sentlist in [treebank.parsed_sents(file_id) for file_id in files] for sent in sentlist]
 print(parsed_sents[0])
+
+grammar = nltk.data.load("project1_grammar.cfg")
+print(grammar)
