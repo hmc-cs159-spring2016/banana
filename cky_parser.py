3fc06b3d00a6b246c1538f76c17523e1e91a42f9
Rowan Zellers <rzellers@hmc.edu>
2016-04-12 20:10:03 -0700

    Bugs

--- cky_parser.py
+++ cky_parser.py
@@ -3,8 +3,8 @@ CKY Parser for constituent parsing
 """
 
 from nltk.corpus import treebank
-from nltk import Nonterminal, nonterminals, Production, CFG
-
+from nltk import Nonterminal, nonterminals, Production, CFG, Tree, ProbabilisticTree
+from functools import reduce
 # Get the Penn Treebank corpus
 files = treebank.fileids()
 files = files[:5] # make shorter for setup
@@ -24,42 +24,51 @@ class ckyparser:
     def parse(self,sent):
         toks = sent.split(' ')
         #Get the first layer
+
+        n = len(toks)
+        
         initlayer = [[p.lhs() for p in self.rules if p.rhs()[0] == x] for x in toks]
-        curlayer = initlayer
-        laterlayers = [curlayer]
-        for i in range(len(initlayer)):
-            newlayer = []
-            for (bs,cs) in zip(curlayer[:-1],curlayer[1:]):
-                for b in bs:
-                    for c in cs:
-                        print(b,c)
-                        newlayer.append([p.lhs() for p in self.rules if p.rhs() == (b,c)])
-            laterlayers.append(newlayer)
-            curlayer = newlayer
         
-        for l in laterlayers[::-1]:
-            print(l)
+        #where 
+        chart = [[[] for x in range(i+1)] for i in range(n)]
+        bts = [[[] for x in range(i+1)] for i in range(n)]
+        for i,elem in enumerate(initlayer):
+            chart[i][i] = elem
         
-        #Unary rules
+        for depth in range(1,n): #iterate along depth n
+            height = n-depth
+            for target in range(height):
+                i = depth + target
+                j = target
+                
+                #List of all relevant things to look at
+                up =  [(i-x,j) for x in range(1,depth+1)]
+                right = [(i,j+x) for x in range(1,depth+1)]
+                
+                allNTs = []
+                allBTs = []
+                for (i1,j1),(i2,j2) in zip(up,right):
+                    
+                    thingstoadd = [p.lhs() for p in self.rules if (p.rhs()[0] in chart[i1][j1] and p.rhs()[1] in chart[i2][j2])]
+                    if len(thingstoadd) > 0:
+                        allBTs.append(((i1,j1),(i2,j2)))
+                    allNTs.append(set(thingstoadd))
+                chart[i][j] = list(reduce(set.union, allNTs))
+                bts[i][j] = allBTs
+                
+        return chart,bts
+        
         
 
 
 
 
-
+#Check it
 nts = nonterminals('S, NP, VP, PP, N, V, P, DT')
-#grammar = CFG.fromstring("""
-#... S -> NP VP
-#... PP -> P NP
-#... NP -> 'the' N | N PP | 'the' N PP
-#... VP -> V NP | V PP | V NP PP
-#... N -> 'cat'
-#... N -> 'dog'
-#... N -> 'rug'
-#... V -> 'chased'
-#... V -> 'sat'
-#... P -> 'in'
-#... P -> 'on' """)
 
-myparser = ckyparser(grammar.productions(),nts)
-myparser.parse("the cat sat on the dog")
+s = '(S (NP (DT the) (NN cat)) (VP (VBD ate) (NP (DT a) (NN cookie))))'
+t = Tree.fromstring(s)
+t.chomsky_normal_form()
+
+myparser = ckyparser(t.productions(),nts)
+myparser.parse("the cat ate a cookie")
