bf0047eea8bbbc963e2dcd0ce2d191d8e6f10b43
Rowan Zellers <rzellers@hmc.edu>
2016-04-20 13:50:30 -0700

    Moved tests out of cky_parser

--- cky_parser.py
+++ cky_parser.py
@@ -8,14 +8,6 @@ from nltk import treetransforms
 from functools import reduce
 from collections import defaultdict
 import nltk
-# Get the Penn Treebank corpus
-files = treebank.fileids()
-files = files[:5] # make shorter for setup
-
-parsed_sents = [sent for sentlist in [treebank.parsed_sents(file_id) for file_id in files] for sent in sentlist]
-print(parsed_sents[0])
-
-
 
 class ckyparser:
     def __init__(self,rules,success):
@@ -119,55 +111,4 @@ class ckyparser:
         mytrees = [t for t in trees[-1][0] if t.label() == self.success]
         
         return chart,mytrees
-    
 
-toy_pcfg1 = PCFG.fromstring("""
-    S -> NP VP [1.0]
-    NP -> Det N [0.5] | NP PP [0.25] | 'John' [0.1] | 'I' [0.15]
-    Det -> 'the' [0.8] | 'my' [0.2]
-    N -> 'man' [0.5] | 'telescope' [0.5]
-    V -> 'ate' [0.35] | 'saw' [0.65]
-    VP -> VP PP [0.1] | V NP [0.7] | 'ate' [0.07] | 'saw' [0.13]
-    PP -> P NP [1.0]
-    P -> 'with' [0.61] | 'under' [0.39]
-    """)
-    
-#toy_pcfg1 = treetransforms.chomsky_normal_form(toy_pcfg1)
-myparser = ckyparser(toy_pcfg1.productions(),Nonterminal('S'))
-chart,mytrees=myparser.probabilistic_parse("I saw John with my telescope")
-
-
-
-
-
-
-
-## Deterministic parse
-
-import chomsky_converter 
-cfg_grammar = nltk.data.load("project1_grammar.cfg")
-cnf_grammar = chomsky_converter.convert_grammar(cfg_grammar)
-
-
-#Check it
-nts = nonterminals('S, NP, VP, PP, N, V, P, DT')
-
-#s = '(S (NP (DT the) (NN cat)) (VP (VBD ate) (NP (DT a) (NN cookie))))'
-#t = Tree.fromstring(s)
-#t.chomsky_normal_form()
-
-myparser = ckyparser(cnf_grammar.productions(),Nonterminal('TOP'))
-
-with open('sentences.txt','r') as f:
-    allexamples = f.read().splitlines()
-for ex in allexamples:
-    chart,mytrees=myparser.deterministic_parse(ex)
-    if len(mytrees)>0:
-        print("success")
-    else:
-        print("fail")
-        print(ex)
-        print(nltk.word_tokenize(ex))
-        print(mytrees)
-        
-        
